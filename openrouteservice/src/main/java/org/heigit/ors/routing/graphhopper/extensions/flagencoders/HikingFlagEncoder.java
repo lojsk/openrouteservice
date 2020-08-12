eb81db53fadc824a509eab2023f5e5cfaf6cf659
Lojsk <jan@oc.si>
2020-08-12 08:03:19 +0200

    change hiking speed base on elevation

--- openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/flagencoders/HikingFlagEncoder.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/flagencoders/HikingFlagEncoder.java
@@ -19,8 +19,10 @@ import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.util.PriorityCode;
 import com.graphhopper.storage.IntsRef;
+import com.graphhopper.util.EdgeIteratorState;
 import com.graphhopper.util.Helper;
 import com.graphhopper.util.PMap;
+import com.graphhopper.util.PointList;
 import org.heigit.ors.routing.graphhopper.extensions.OSMTags;
 
 import java.util.Arrays;
@@ -61,14 +63,14 @@ public class HikingFlagEncoder extends FootFlagEncoder {
                 "footway"
         ));
 
-        sacScaleSpeeds.put("hiking", 4.0);
-        sacScaleSpeeds.put("mountain_hiking", 2.5);
-        sacScaleSpeeds.put("demanding_mountain_hiking", 1.1);
-        sacScaleSpeeds.put("alpine_hiking", 1.0);
-        sacScaleSpeeds.put("demanding_alpine_hiking", 0.9);
-        sacScaleSpeeds.put("difficult_alpine_hiking", 0.8);
+        sacScaleSpeeds.put("hiking", 5.0);
+        sacScaleSpeeds.put("mountain_hiking", 4.5);
+        sacScaleSpeeds.put("demanding_mountain_hiking", 1.8);
+        sacScaleSpeeds.put("alpine_hiking", 1.4);
+        sacScaleSpeeds.put("demanding_alpine_hiking", 1.2);
+        sacScaleSpeeds.put("difficult_alpine_hiking", 1.2);
 
-        speedDefault = 4.0;
+        speedDefault = 5.0;
         init();
     }
 
@@ -109,6 +111,41 @@ public class HikingFlagEncoder extends FootFlagEncoder {
         }
 
         return speedDefault;
+    }
+
+    @Override
+    public void applyWayTags(ReaderWay way, EdgeIteratorState edge) {
+        PointList pl = edge.fetchWayGeometry(3);
+        if (!pl.is3D())
+            return;
+
+        if (way.hasTag("tunnel", "yes") || way.hasTag("bridge", "yes") || way.hasTag("highway", "steps"))
+            // do not change speed
+            // note: although tunnel can have a difference in elevation it is unlikely that the elevation data is correct for a tunnel
+            return;
+
+        // Decrease the speed for ele increase (incline), and slightly decrease the speed for ele decrease (decline)
+        double prevEle = pl.getElevation(0);
+        double fullDistance = edge.getDistance();
+
+        // for short edges an incline makes no sense and for 0 distances could lead to NaN values for speed, see #432
+        if (fullDistance < 2)
+            return;
+
+        double eleDelta = Math.abs(pl.getElevation(pl.size() - 1) - prevEle);
+        double slope = eleDelta / fullDistance;
+
+        IntsRef edgeFlags = edge.getFlags();
+        if ((accessEnc.getBool(false, edgeFlags) || accessEnc.getBool(true, edgeFlags))
+                && slope > 0.005) {
+
+            // see #1679 => v_hor=4.5km/h for horizontal speed; v_vert=2*0.5km/h for vertical speed (assumption: elevation < edge distance/4.5)
+            // s_3d/v=h/v_vert + s_2d/v_hor => v = s_3d / (h/v_vert + s_2d/v_hor) = sqrt(s²_2d + h²) / (h/v_vert + s_2d/v_hor)
+            // slope=h/s_2d=~h/2_3d              = sqrt(1+slope²)/(slope+1/4.5) km/h
+            // maximum slope is 0.37 (Ffordd Pen Llech)
+            double newSpeed = Math.sqrt(1 + slope * slope) / (slope + 1 / getSpeed(way));
+            edge.set(speedEncoder, Helper.keepIn(newSpeed, 0.3, speedDefault));
+        }
     }
 
 }
